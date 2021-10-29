b554e756c0eef9f75986790020fe79066aca2438
Andy Yang <yzwdroid@gmail.com>
2021-10-29 14:28:06 -0400

    add comments

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"os"
 )
 
+// Create a program which lists environment variables as '=' delimited key/value pairs:
 func main() {
+	// Environ returns a copy of strings([]string) representing the environment,
+	// in the form "key=value".
 	for _, e := range os.Environ() {
 		fmt.Println(e)
 	}
